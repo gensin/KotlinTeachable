e37c9078d86ab8b77f613d50d9eb0460f89b4b53
Pau Arlandis Martinez <ghostdesert@gmail.com>
2017-07-29 03:00:36 +0200

    second week final exercise

--- app/src/main/java/es/pau/kotlinteachable/MainActivity.kt
+++ app/src/main/java/es/pau/kotlinteachable/MainActivity.kt
@@ -2,6 +2,8 @@ package es.pau.kotlinteachable
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,21 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        changeText("hello Textview")
-        toast("Hello Toast")
+        val message: EditText = findViewById(R.id.message) as EditText
+        val button: Button = findViewById(R.id.button) as Button
+        button.setOnClickListener {
+            if (message.text.toString().isEmpty()){
+                toast("Escribe algo!")
+            } else {
+                toast("Hello ${message.text}")
+            }
+        }
     }
 
-    private fun changeText(text: String) {
-        val textView = findViewById(R.id.text_view) as TextView
-        // Existen dos formas de agregar texto en un text view
-//        textView.setText(text)
-        textView.text = text
-    }
-
-    // Two ways of create functions. One as a typical method
-    private fun toast(message: String) { // functions on kotlin always return somethign, 'Unit' by default
+    private fun toast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
-    // Other way as an expression with the body of the method after '='
-    fun getIntx2(int: Int) = 2 * int // Returned type (Int) is implicit on the body of the function
 }
