7de00fa8f5ca86ca7a17268384f5f6d763a553ad
Pau <ghostdesert@gmail.com>
2017-08-09 08:53:42 +0200

    add enum

--- app/src/main/java/es/pau/kotlinteachable/MediaItem.kt
+++ app/src/main/java/es/pau/kotlinteachable/MediaItem.kt
@@ -4,5 +4,7 @@ package es.pau.kotlinteachable
  * Project: KotlinTeachable
  * Created on 03/08/2017.
  */
-data class MediaItem(val title: String, val thumbUrl: String) {
+data class MediaItem(val title: String, val thumbUrl: String, val type: Type) {
+
+    enum class Type { PHOTO, VIDEO }
 }
