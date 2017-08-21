c3b21629879ab5de5f4c2eb563db9e14f759f2ea
pau <parlandis@drive-smart.com>
2017-08-21 13:46:43 +0200

    add property delegation with lazy example in main

--- app/src/main/java/es/pau/kotlinteachable/MainActivity.kt
+++ app/src/main/java/es/pau/kotlinteachable/MainActivity.kt
@@ -2,13 +2,17 @@ package es.pau.kotlinteachable
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val recyclerView by lazy {
+        findViewById(R.id.recycler) as RecyclerView
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recycler.adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
+        recyclerView.adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
     }
 }
