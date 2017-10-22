bd16f6eb5fbc3e3a9e535507909b5a0f3934796b
Pau <ghostdesert@gmail.com>
2017-10-22 02:32:09 +0200

    add ranges

--- app/src/main/java/es/pau/kotlinteachable/GetMedia.kt
+++ app/src/main/java/es/pau/kotlinteachable/GetMedia.kt
@@ -5,15 +5,12 @@ package es.pau.kotlinteachable
  */
 private val thumbBase = "http://lorempixel.com/400/400/cats/"
 
-fun getMedia() = listOf(
-    MediaItem("Title 1", "${thumbBase}1", MediaItem.Type.PHOTO),
-    MediaItem("Title 2", "${thumbBase}2", MediaItem.Type.PHOTO),
-    MediaItem("Title 3", "${thumbBase}3", MediaItem.Type.VIDEO),
-    MediaItem("Title 4", "${thumbBase}4", MediaItem.Type.PHOTO),
-    MediaItem("Title 5", "${thumbBase}5", MediaItem.Type.PHOTO),
-    MediaItem("Title 6", "${thumbBase}6", MediaItem.Type.VIDEO),
-    MediaItem("Title 7", "${thumbBase}7", MediaItem.Type.VIDEO),
-    MediaItem("Title 8", "${thumbBase}8", MediaItem.Type.PHOTO),
-    MediaItem("Title 9", "${thumbBase}9", MediaItem.Type.PHOTO),
-    MediaItem("Title 10", "${thumbBase}10", MediaItem.Type.VIDEO)
-)
+fun getMedia() = (1..10).map{ MediaItem("Title $it", "$thumbBase$it", selectType(it)) }
+
+fun selectType(position: Int): MediaItem.Type {
+    if (position % 3 == 0){
+        return MediaItem.Type.VIDEO
+    }
+
+    return MediaItem.Type.PHOTO
+}
